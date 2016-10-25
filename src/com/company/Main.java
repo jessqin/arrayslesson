f2c3a520fbbadde05848f4ac2901b824e8087cd9
jessqin <jessicaq319@gmail.com>
2016-10-25 14:09:19 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //use indices to tell arrays apart;
+        //declare array by int [] a =... or int a [] = ...
+        int a [] = new int[400];
+        a[0] = 97;
+        a[1] = 62;
+        a[2] = 85;
+
+        //another way
+        //int a[] = [97,62,85];
+
+        //find the length of an array
+
+        double b[] = new double[7];
+        int lngth = b.length;
+        System.out.println(lngth);
+
+        //parallel arrays
+
+        //primitive vs reference(objects)
+        
+    }
+}
